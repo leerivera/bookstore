ad5bec7eaff3db61c9d5d09a5dfbe96b17bbdc6b
leerivera <leerivera80@icloud.com>
2021-01-28 21:45:13 -0500

    user schema created

--- bookstoreecom/models/user.js
+++ bookstoreecom/models/user.js
@@ -38,3 +38,35 @@ const userSchema = new mongosse.Mongoose.Schema({
     },
     { timestamps: true }
 };
+
+// virtual field
+
+userSchema.virtual('password')
+//sets password
+.set(function(password){
+    this._password = password
+    this.salt = uuidv1()
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function() {
+    return this._password
+})
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if(!password) return '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                    .update(password)
+                    .digest('hex')
+        } catch(error) {
+            return "";
+        }
+        
+    }
+        
+};
+
+
+module.exports = mongosse.model("User", userSchema);
+
